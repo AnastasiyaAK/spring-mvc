e42a087f336d4185f30c384cd17aa4afeaff5791
AnastasiaAK <asyapetya11@gmail.com>
2023-08-14 21:39:56 +0300

    add fixes

--- src/main/java/by/akulich/gcs/config/SpringConfig.java
+++ src/main/java/by/akulich/gcs/config/SpringConfig.java
@@ -11,6 +11,7 @@ import javax.sql.DataSource;
 @Configuration
 @ComponentScan("by.akulich.gcs")
 public class SpringConfig {
+
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
--- src/main/java/by/akulich/gcs/controllers/GiftCertificatesController.java
+++ src/main/java/by/akulich/gcs/controllers/GiftCertificatesController.java
@@ -1,42 +1,31 @@
 package by.akulich.gcs.controllers;
 
 import by.akulich.gcs.dao.GiftCertificateDAO;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/gift-certificates")
 public class GiftCertificatesController {
 
     private final GiftCertificateDAO giftCertificateDAO;
-    private final ObjectMapper objectMapper= new ObjectMapper();
 
     @Autowired
     public GiftCertificatesController(GiftCertificateDAO giftCertificateDAO) {
         this.giftCertificateDAO = giftCertificateDAO;
     }
 
-    @GetMapping()
+    @GetMapping("/all")
     public String getAllGiftCertificates() {
-        try {
-            return objectMapper.writeValueAsString(giftCertificateDAO.getAll());
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+        return giftCertificateDAO.getAll().toString();
     }
 
     @GetMapping("/{name}")
     public String getGiftCertificateByName(@PathVariable String name) {
-        try {
-            return objectMapper.writeValueAsString(giftCertificateDAO.getByName(name));
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+        return String.valueOf(giftCertificateDAO.getByName(name));
     }
 
 }
